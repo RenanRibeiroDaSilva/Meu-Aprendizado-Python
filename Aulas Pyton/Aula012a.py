43fdbb1754b18668aaae88d114daca21b5aaf61b
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2021-06-04 13:25:50 -0300

    Aula012a - Dicas e Regras
    Curso em Vídeo

---
+++ Aulas Pyton/Aula012a.py
@@ -0,0 +1,6 @@
+"""         Aula - 011 - Dicas e Regras para estudar"""
+
+# Mundo 2 do curso de Python feito pelo Curso em Video com professor Gustavo Guanabara
+'''Aqui é uma aula onde o foco do professor Guanabara foi em dar dicas e conselhos 
+de como estudar. Para que possamos tirar maior proveito do curso de python. 
+    Já nos deu uma ideia do foco do mundo dois.'''
