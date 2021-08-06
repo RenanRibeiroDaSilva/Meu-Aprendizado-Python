1182428253710ea7732e132d01edabc238a4bd88
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2021-08-06 14:53:27 -0300

    Ex110 - Reduzindo ainda mais seu programa
    Curso em Vídeo

---
+++ Exercicios/Ex110/teste.py
@@ -0,0 +1,12 @@
+"""     Ex - 110 -  Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada
+        resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no
+        módulo criado até aqui.
+"""
+
+import moeda
+
+# Programa principal:
+val = float(input('Digite um valor: R$'))
+
+# Imprimir na tela para o usuário:
+moeda.resumo(val)
