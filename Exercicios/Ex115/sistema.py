98c648a8d37e7d2187d9b6e48335039601f8b12b
Renan Ribeiro <84098891+RenanRibeiroDaSilva@users.noreply.github.com>
2021-08-10 14:47:29 -0300

    Ex115 - Arquivos com Python
    Curso em Vídeo

--- Exercicios/Ex115/sistema.py
+++ Exercicios/Ex115/sistema.py
@@ -1,12 +1,20 @@
 from Ex115.lib.interface import *
+from Ex115.lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
 
 cabecalho('SISTEMA ARQUIVO v1.0')
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        cabecalho('Opc1')
+        # Opção de listar o conteudo de um arquivo
+        lerArquivo(arq)
     elif resposta == 2:
         cabecalho('Opc2')
     elif resposta == 3:
